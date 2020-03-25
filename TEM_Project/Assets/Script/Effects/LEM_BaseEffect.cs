c5e3323359b627167202269e7ae082817d8a3c1d
bestcolour <49665281+bestcolour@users.noreply.github.com>
2020-03-25 23:10:35 +0800

    Revert "Added Small Features"
    This reverts commit c0a143cbf16add77ab8e11cce3b388726a95b48a.

--- TEM_Project/Assets/Script/Effects/LEM_BaseEffect.cs
+++ TEM_Project/Assets/Script/Effects/LEM_BaseEffect.cs
@@ -12,13 +12,17 @@ namespace LEM_Effects
     [Serializable]
     public class LEM_BaseEffect :ScriptableObject
     {
+
         //Records the node type this effect belongs to
-        [ReadOnly] public string m_NodeEffectType = default;
+        public string m_NodeEffectType = default;
 
         [Tooltip("Write a summary about what this event does if you want to"), TextArea(3, 5)]
         public string m_Description = default;
 
-        [Tooltip("Stores basic node data. This is applicable for effect nodes as well"), Header("Node Data")]
+        //[Tooltip("Basically holds what should be the next event to trigger")]
+        ////public LEM_BaseEffect m_NextEffect = default;
+        //public string m_NextEffectNodeID = default;
+
         public NodeBaseData m_NodeBaseData = default;
 
 
