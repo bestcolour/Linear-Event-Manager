5e2cbd6955b6b86684057eaa661c22650fffa1ee
bestcolour <49665281+bestcolour@users.noreply.github.com>
2020-05-01 13:29:02 +0800

    Addedb BoolHideIf attribute
    Which determines whether or not a field gets drawn based on a bool value of another field. Works for array and lists

---
+++ TEM_Project/Assets/Editor/PropertyDrawers/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyPropertyDrawer : PropertyDrawer
+{
+
+#if UNITY_EDITOR
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //base.OnGUI(position, property, label);
+        bool prevEnabledState = GUI.enabled;
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label);
+        GUI.enabled = prevEnabledState;
+    }
+#endif
+
+}
+
---
+++ TEM_Project/Assets/LEM_Scripts/Testing/TestingCondiitonalHide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingCondiitonalHide : MonoBehaviour
+{
+    public bool m_HideConditionalField = true;
+
+    [SerializeField, BoolHideIf("m_HideConditionalField",isFullPropertyPath: true,m_StateToObserve = false)]
+    List<int> m_SlaveOne;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
