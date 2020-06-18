21778c4298b506e4e7b149053bd888cee4975d52
bestcolour <49665281+bestcolour@users.noreply.github.com>
2020-06-18 16:53:55 +0800

    Safety push aft deletion of snapshot

--- TEM_Project/Library/Collab/Download/Assets/LEM_Scripts/Utility/Attributes/ReadOnlyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿#if UNITY_EDITOR
-using System;
-using UnityEngine;
-
-[AttributeUsage(AttributeTargets.Field, Inherited = true)]
-public class ReadOnlyAttribute : PropertyAttribute { }
-
-
-#endif
