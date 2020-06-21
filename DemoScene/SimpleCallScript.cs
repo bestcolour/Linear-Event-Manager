746daa31fbb22036df33c2d71bf014f7d04eea4f
bestcolour <49665281+bestcolour@users.noreply.github.com>
2020-06-21 15:07:30 +0800

    Added demo scene

---
+++ DemoScene/SimpleCallScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCallScript : MonoBehaviour
+{
+    //Use LinearDescription attribute to make displaying LinearEvent's description easier to see
+    [SerializeField,LinearDescription]
+    LinearEvent m_TargetLinearEvent = default;
+
+    private void Start()
+    {
+        m_TargetLinearEvent.InitializeLinearEvent();
+        m_TargetLinearEvent.PlayLinearEvent();
+    }
+
+
+}
